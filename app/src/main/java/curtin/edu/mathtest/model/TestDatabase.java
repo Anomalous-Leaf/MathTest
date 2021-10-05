ca12c5d9b519b3a48154a0b9412f93d95a8f294c
Zenith <exdragon01@gmail.com>
2021-10-05 11:24:10 +0800

    Created basic outline of TestDatabase

--- app/src/main/java/curtin/edu/mathtest/model/TestDatabase.java
+++ app/src/main/java/curtin/edu/mathtest/model/TestDatabase.java
@@ -1,10 +1,77 @@
 package curtin.edu.mathtest.model;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestDatabase
 {
+    private SQLiteDatabase db;
+
     private TestDatabase()
     {
 
     }
+
+    public void load(Context context)
+    {
+        this.db = new TestDbHelper(context.getApplicationContext()).getWritableDatabase();
+    }
+
+    public void addResult()
+    {
+
+    }
+
+    public void addStudent(Student student)
+    {
+
+    }
+
+    public List<TestResult> getResults(int id)
+    {
+        List<TestResult> results = new ArrayList<>();
+
+        return results;
+
+    }
+
+    public List<Student> getStudents()
+    {
+        return null;
+    }
+
+    private class TestCursor extends CursorWrapper
+    {
+        public TestCursor(Cursor cursor)
+        {
+            super(cursor);
+        }
+
+        public Student getStudent()
+        {
+            return null;
+        }
+
+        public TestResult getTestResult()
+        {
+            return null;
+        }
+
+        public List<String> getPhoneList()
+        {
+            return null;
+        }
+
+        public List<String> getEmailList()
+        {
+            return null;
+        }
+    }
+
 
 }
