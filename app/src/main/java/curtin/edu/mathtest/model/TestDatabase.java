15ca0e5deebf99ef894bf5a1e72f29f030c8820c
Zenith <exdragon01@gmail.com>
2021-10-05 12:04:21 +0800

    AddStudent and AddResult methods of database complete

--- app/src/main/java/curtin/edu/mathtest/model/TestDatabase.java
+++ app/src/main/java/curtin/edu/mathtest/model/TestDatabase.java
@@ -1,9 +1,11 @@
 package curtin.edu.mathtest.model;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 import android.database.sqlite.SQLiteDatabase;
+import static curtin.edu.mathtest.model.TestDatabaseSchema.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +24,51 @@ public class TestDatabase
         this.db = new TestDbHelper(context.getApplicationContext()).getWritableDatabase();
     }
 
-    public void addResult()
+    public void addResult(TestResult result)
     {
+        ContentValues cv = new ContentValues();
 
+        //Insert values into ContentValues
+        cv.put(TestResults.Cols.ID, result.getStudentId());
+        cv.put(TestResults.Cols.START_TIME, result.getStartTime());
+        cv.put(TestResults.Cols.END_TIME, result.getEndTime());
+        cv.put(TestResults.Cols.NUM_QUESTIONS, result.getQuestions());
+        cv.put(TestResults.Cols.SCORE, result.getScore());
+
+        db.insert(TestResults.NAME, null, cv);
     }
 
     public void addStudent(Student student)
     {
+        ContentValues studentCv = new ContentValues();
+        ContentValues phoneCv;
+        ContentValues emailCv = new ContentValues();
 
+        //Put student data into cv
+        studentCv.put(StudentTable.Cols.ID, student.getId());
+        studentCv.put(StudentTable.Cols.FIRST_NAME, student.getFirstName());
+        studentCv.put(StudentTable.Cols.LAST_NAME, student.getLastName());
+        studentCv.put(StudentTable.Cols.PHOTO_URI, student.getPhotoUri());
+
+        db.insert(StudentTable.NAME, null, studentCv);
+
+        //Loop to insert all phone numbers. Each loop is one row
+        for (String number : student.getPhoneList())
+        {
+            phoneCv = new ContentValues();
+            phoneCv.put(PhoneTable.Cols.PHONE, number);
+            phoneCv.put(PhoneTable.Cols.ID, student.getId());
+            db.insert(PhoneTable.NAME, null, phoneCv);
+        }
+
+        //Loop to insert all emails. Each loop is a row
+        for (String email : student.getEmailList())
+        {
+            emailCv = new ContentValues();
+            emailCv.put(EmailTable.Cols.EMAIL, email);
+            emailCv.put(EmailTable.Cols.ID, student.getId());
+            db.insert(EmailTable.NAME, null, emailCv);
+        }
     }
 
     public List<TestResult> getResults(int id)
