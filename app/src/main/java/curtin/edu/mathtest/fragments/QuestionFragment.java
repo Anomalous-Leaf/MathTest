b14b18daee43c010d40d7dd3c16f145dcb120492
Zenith <exdragon01@gmail.com>
2021-10-05 20:23:40 +0800

    Completed Answer and Question layouts

--- app/src/main/java/curtin/edu/mathtest/fragments/QuestionFragment.java
+++ app/src/main/java/curtin/edu/mathtest/fragments/QuestionFragment.java
@@ -61,6 +61,6 @@ public class QuestionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_frgment, container, false);
+        return inflater.inflate(R.layout.fragment_question_fragment, container, false);
     }
 }
